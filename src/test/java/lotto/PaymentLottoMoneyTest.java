23c852225f03cdd7d28e9106d63616c92d863f48
Youngho <cyh6099@gmail.com>
2022-11-10 14:31:41 +0900

    test : 로또 구입 금액 유효성 검사, 구입 개수 계산 테스트 작성

---
+++ src/test/java/lotto/PaymentLottoMoneyTest.java
@@ -0,0 +1,63 @@
+package lotto;
+
+import lotto.utils.ExceptionMessages;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("로또 구입 금액, 구입 개수 테스트")
+class PaymentLottoMoneyTest {
+    @Nested
+    @DisplayName("로또 구입 금액 유효성 예외테스트")
+    class PaymentLottoMoneyExceptionTest {
+        @Test
+        @DisplayName("로또 구입 금액에 숫자가 아닌 것이 있으면 예외 발생")
+        void hasNotNumberExceptionTest() {
+            IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> new PaymentLottoMoney("1000 "));
+            assertThat(exception.getMessage()).isEqualTo(ExceptionMessages.PAYMENT_ONLY_NUMBER);
+        }
+
+        @Test
+        @DisplayName("로또구입금액이 천원 단위가 아니면 예외발생")
+        void notThousandUnitExceptionTest() {
+            IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> new PaymentLottoMoney("1100"));
+            assertThat(exception.getMessage()).isEqualTo(ExceptionMessages.PAYMENT_ONLY_ONE_THOUSAND_UNIT);
+        }
+
+        @Test
+        @DisplayName("로또구입금액이 천원 미만이면 예외발생")
+        void lessThanThousandExceptionTest() {
+            IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> new PaymentLottoMoney("900"));
+            assertThat(exception.getMessage()).isEqualTo(ExceptionMessages.PAYMENT_ONLY_ONE_THOUSAND_UNIT);
+        }
+    }
+
+    @Nested
+    @DisplayName("로또 구입 개수 반환 테스트")
+    class CountNumberOfBuyLottoTest {
+        @Test
+        @DisplayName("로또구입 개수 테스트")
+        void calculateNumberOfLotto() {
+            PaymentLottoMoney paymentLottoMoney = new PaymentLottoMoney("10000");
+            int lottoCount = paymentLottoMoney.calculateNumberOfLotto();
+            int expectedLottoCount = 10;
+            assertThat(lottoCount).isEqualTo(expectedLottoCount);
+        }
+
+        @Test
+        @DisplayName("로또 한개 구입 개수 테스트")
+        void buyOneLottoTest() {
+            PaymentLottoMoney paymentLottoMoney = new PaymentLottoMoney("1000");
+            int lottoCount = paymentLottoMoney.calculateNumberOfLotto();
+            int expectedLottoCount = 1;
+            assertThat(lottoCount).isEqualTo(expectedLottoCount);
+        }
+    }
+
+}
