725add847ebb074d11f3ea0ef275d8322a68c0b7
Youngho <cyh6099@gmail.com>
2022-11-10 14:43:07 +0900

    feat : 6자리 로또 생성 시 유효성 검사 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,44 @@
 package lotto;
 
+import lotto.utils.ExceptionMessages;
 import java.util.List;
 
 public class Lotto {
+    private final int CORRECT_LOTTO_NUMBER_COUNT = 6;
+    private final int LOTTO_MIN_NUMBER = 1;
+    private final int LOTTO_MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
+        validate();
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validate() {
+        isCorrectLottoNumberCount();
+        isUniqueLottoNumbers();
+        isSatisfiedLottoNumberRange();
+    }
+
+    private void isCorrectLottoNumberCount() {
+        if (numbers.size() != CORRECT_LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessages.LOTTO_ONLY_SIX_SIZE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void isUniqueLottoNumbers() {
+        if (numbers.stream()
+                .distinct()
+                .count() != CORRECT_LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessages.LOTTO_ONLY_UNIQUE_NUMBERS);
+        }
+    }
+
+    private void isSatisfiedLottoNumberRange() {
+        boolean isOutOfLottoNumberRange = numbers.stream()
+                .anyMatch(lottoNumber -> lottoNumber < LOTTO_MIN_NUMBER || lottoNumber > LOTTO_MAX_NUMBER);
+        if (isOutOfLottoNumberRange) {
+            throw new IllegalArgumentException(ExceptionMessages.LOTTO_OUT_OF_RANGE);
+        }
+    }
 }
--- src/main/java/lotto/utils/ExceptionMessages.java
+++ src/main/java/lotto/utils/ExceptionMessages.java
@@ -3,4 +3,7 @@ package lotto.utils;
 public class ExceptionMessages {
     public static String PAYMENT_ONLY_NUMBER = "[ERROR] 구입 금액은 숫자만 올 수 있습니다.";
     public static String PAYMENT_ONLY_ONE_THOUSAND_UNIT = "[ERROR] 구입 금액은 천원 단위만 가능합니다.";
+    public static String LOTTO_ONLY_SIX_SIZE = "[ERROR] 로또는 6개의 숫자로만 이루어집니다.";
+    public static String LOTTO_ONLY_UNIQUE_NUMBERS = "[ERROR] 로또에는 중복된 숫자가 올 수 없습니다.";
+    public static String LOTTO_OUT_OF_RANGE = "[ERROR] 로또 숫자는 1~45까지만 가능합니다.";
 }
