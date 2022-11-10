7b99c54de1ef321a53197fc63209102e0bef9613
Youngho <cyh6099@gmail.com>
2022-11-10 14:33:07 +0900

    feat : 로또 구입 금액 유효성 검사, 구입 개수 계산 기능 구현

---
+++ src/main/java/lotto/PaymentLottoMoney.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import lotto.utils.ExceptionMessages;
+
+public class PaymentLottoMoney {
+    private final String PAYMENT_NUMBER_REGEX = "^[0-9]*$";
+    private final int LOTTO_PRICE = 1000;
+    private int paymentMoney;
+
+    public PaymentLottoMoney(String paymentMoney) {
+        isPaymentOnlyNumber(paymentMoney);
+        isPaymentOneThousandUnit(toIntPaymentMoney(paymentMoney));
+        this.paymentMoney = toIntPaymentMoney(paymentMoney);
+    }
+
+    private void isPaymentOnlyNumber(String paymentMoney) {
+        if (!paymentMoney.matches(PAYMENT_NUMBER_REGEX)) {
+            throw new IllegalArgumentException(ExceptionMessages.PAYMENT_ONLY_NUMBER);
+        }
+    }
+
+    private int toIntPaymentMoney(String paymentMoney) {
+        return Integer.parseInt(paymentMoney);
+    }
+
+    private void isPaymentOneThousandUnit(int paymentMoney) {
+        if (paymentMoney % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ExceptionMessages.PAYMENT_ONLY_ONE_THOUSAND_UNIT);
+        }
+    }
+
+    public int calculateNumberOfLotto() {
+        return paymentMoney / LOTTO_PRICE;
+    }
+}
---
+++ src/main/java/lotto/utils/ExceptionMessages.java
@@ -0,0 +1,6 @@
+package lotto.utils;
+
+public class ExceptionMessages {
+    public static String PAYMENT_ONLY_NUMBER = "[ERROR] 구입 금액은 숫자만 올 수 있습니다.";
+    public static String PAYMENT_ONLY_ONE_THOUSAND_UNIT = "[ERROR] 구입 금액은 천원 단위만 가능합니다.";
+}
