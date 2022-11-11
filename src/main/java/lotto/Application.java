dd1bfcc4368b9392cfd884fbb2fffe70d8730102
Youngho <cyh6099@gmail.com>
2022-11-11 16:50:49 +0900

    refactor : BuyLottoController에서 EntireLotto, PaymentLottoMoney로 향하는 연관관계를 의존관계로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import lotto.view.inputView.MoneyInputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BuyLottoController buyLottoController = new BuyLottoController(new EntireLotto());
-        EntireLotto entireLotto = buyLottoController.BuyLotto();
+        BuyLottoController buyLottoController = new BuyLottoController();
+        String paymentMoney = MoneyInputView.inputMoney();
+        EntireLotto entireLotto = buyLottoController.BuyLotto(new PaymentLottoMoney(paymentMoney), new EntireLotto());
         entireLotto.showEntireLottoInfo();
     }
 }
--- src/main/java/lotto/BuyLottoController.java
+++ src/main/java/lotto/BuyLottoController.java
@@ -1,18 +1,7 @@
 package lotto;
 
-import lotto.view.inputView.MoneyInputView;
-
 public class BuyLottoController {
-    PaymentLottoMoney paymentLottoMoney;
-    EntireLotto entireLotto;
-
-    public BuyLottoController(EntireLotto entireLotto) {
-        String money = MoneyInputView.inputMoney();
-        paymentLottoMoney = new PaymentLottoMoney(money);
-        this.entireLotto = entireLotto;
-    }
-
-    public EntireLotto BuyLotto() {
+    public EntireLotto BuyLotto(PaymentLottoMoney paymentLottoMoney, EntireLotto entireLotto) {
         int lottoCount = paymentLottoMoney.calculateNumberOfLotto();
         entireLotto.generateEntireLotto(lottoCount);
         return entireLotto;
