15c3dcc39fb02cf93deeb96df972bb682ea6cb80
Youngho <cyh6099@gmail.com>
2022-11-12 14:02:55 +0900

    test : 당첨번호와 각 로또를 비교해 당첨 순위 구하는 기능 테스트 작성, 추후 출력 형식 리팩토링 필요

--- src/test/java/lotto/EntireLottoTest.java
+++ src/test/java/lotto/EntireLottoTest.java
@@ -38,6 +38,39 @@ class EntireLottoTest extends NsTest {
         );
     }
 
+    @Test
+    void 순위_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "_1ST 1",
+                            "_2ND 0",
+                            "_3RD 0",
+                            "_4TH 0",
+                            "_5TH 1"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
