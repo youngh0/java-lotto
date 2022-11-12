0938a81ba3e967a709b5970d410390f0b881e16b
Youngho <cyh6099@gmail.com>
2022-11-12 15:48:26 +0900

    refactor : 출력 형식과 맞지 않는 출력 문구 수정

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,18 +1,11 @@
 package lotto;
 
 public enum Ranking {
-    _5TH(3, false, 2_000_000_000, "3개 일치 (5, 000)"),
-    _4TH(4, false, 30_000_000, "4개 일치 (50, 000)"),
-    _3RD(5, false, 1_500_000, "5개 일치 (1, 500, 000)"),
-    _2ND(5, true, 50_000, "5개 일치, 보너스 볼 일치 (30, 000, 000)"),
-    _1ST(6, false, 5_000, "6개 일치 (2, 000, 000, 000)");
-    /**
-     * 3개 일치 (5, 000) - n개
-     * 4개 일치 (50, 000) - n개
-     * 5개 일치 (1, 500, 000) - n개
-     * 5개 일치, 보너스 볼 일치 (30, 000, 000) - n개
-     * 6개 일치 (2, 000, 000, 000) - n개
-     */
+    _5TH(3, false, 5_000, "3개 일치 (5,000원)"),
+    _4TH(4, false, 50_000, "4개 일치 (50,000원)"),
+    _3RD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    _2ND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    _1ST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)");
 
     private int correctNumberCount;
     private boolean isBonus;
@@ -36,5 +29,9 @@ public enum Ranking {
 
     public String getPrintFormat() {
         return printFormat;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
