7838f28e1f05aac6e9013759b5555d1050b4af39
Youngho <cyh6099@gmail.com>
2022-12-02 15:41:58 +0900

    fix : 당첨번호 입력 시 유효성 검사 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,13 +1,39 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumbers {
     private final Lotto winningNumbers;
     private final int bonusNumber;
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 45;
 
-    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = new Lotto(winningNumbers);
+    public WinningNumbers(String winningNumbers, int bonusNumber) {
+        this.winningNumbers = new Lotto(convertWinningNumber(winningNumbers));
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private List<Integer> convertWinningNumber(String winningNumber) {
+        List<Integer> result = new ArrayList<>();
+        for (String number : winningNumber.split(",")) {
+            result.add(convertOneWinningNumber(number));
+        }
+        return result;
+    }
+
+    private Integer convertOneWinningNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validate(int bonusNumber) {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException();
+        }
     }
 }
