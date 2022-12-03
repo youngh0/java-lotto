cbfbe21c46a1e3e6f1cbde836227239a713ffcda
Youngho <cyh6099@gmail.com>
2022-12-03 14:32:37 +0900

    feat : 당첨번호, 보너스 번호 입력하는 흐름 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,24 @@
+package lotto.controller;
+
+import lotto.domain.EntireLotto;
+import lotto.domain.WinningNumbers;
+import lotto.dto.PurchaseAmountDTO;
+import lotto.utils.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        EntireLotto entireLotto = new EntireLotto(
+                new PurchaseAmountDTO(inputView.inputMoney()).getMoney() / 1000,
+                new LottoGenerator());
+        outputView.showEntireLottoInfo(entireLotto);
+        System.out.println(entireLotto.getSize());
+        WinningNumbers winningNumbers = new WinningNumbers(
+                inputView.inputWinningNumber(),
+                Integer.parseInt(inputView.inputBonusNumber()));
+    }
+}
