bcec0f33a20118ff33b3eb426ad8a0a048ca11f8
Youngho <cyh6099@gmail.com>
2022-12-03 15:34:17 +0900

    feat : 수익률 출력 형식에 맞게 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,13 +4,17 @@ import lotto.domain.EntireLotto;
 import lotto.domain.LottoResult;
 import lotto.utils.Ranking;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class OutputView {
     public void showEntireLottoInfo(EntireLotto entireLotto) {
         System.out.println(entireLotto.getSize() + "개를 구매했습니다.");
         System.out.println(entireLotto.getEntireLottoInfo());
     }
 
-    public void showRankingResult(LottoResult lottoResult) {
+    public void showRankingResult(LottoResult lottoResult, BigDecimal yield) {
         StringBuffer rankingResult = new StringBuffer();
         for (Ranking value : Ranking.values()) {
             if (value == Ranking.EMPTY) {
@@ -18,6 +22,9 @@ public class OutputView {
             }
             rankingResult.append(value.getMessage(lottoResult.getWinningRankingCount(value))).append("\n");
         }
+        BigDecimal finalYield = yield.setScale(2, RoundingMode.HALF_UP);
+        DecimalFormat yieldFormat = new DecimalFormat("#,##0.0");
+        rankingResult.append("총 수익률은 ").append(yieldFormat.format(finalYield)).append("%입니다.").append("\n");
         System.out.println(rankingResult);
     }
 }
