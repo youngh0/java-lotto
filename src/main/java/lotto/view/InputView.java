be58fd2d09e8b5900df23ea47b527f9a014ef6f0
Youngho <cyh6099@gmail.com>
2022-12-03 14:17:05 +0900

    feat : 사용자 입력 view구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputMoney() {
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        return Console.readLine();
+    }
+}
